6c4689436bbe289bb9905e82b8e338c5f6287cc0
Leorev01 <revrennaleo@gmail.com>
2024-11-14 10:58:11 +0000

    Changes footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import { JSX, SVGProps } from "react"
+import Link from 'next/link'
 
 const navigation = {
   main: [
@@ -23,7 +24,7 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: '',
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -36,7 +37,7 @@ const navigation = {
     },
     {
       name: 'X',
-      href: '#',
+      href: '',
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
@@ -45,7 +46,7 @@ const navigation = {
     },
     {
       name: 'YouTube',
-      href: '#',
+      href: '',
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -65,17 +66,17 @@ export default function Example() {
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
           {navigation.main.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-white">
+            <Link key={item.name} href={item.href} className="text-gray-400 hover:text-white">
               {item.name}
-            </a>
+            </Link>
           ))}
         </nav>
         <div className="mt-16 flex justify-center gap-x-10">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
+            <Link key={item.name} href={item.href} className="text-gray-400 hover:text-gray-300">
               <span className="sr-only">{item.name}</span>
               <item.icon aria-hidden="true" className="h-6 w-6" />
-            </a>
+            </Link>
           ))}
         </div>
         <p className="mt-10 text-center text-sm/6 text-gray-400">&copy; 2024 SJD Exterior Cleaning & Roof Repairs LTD. All rights reserved.</p>
